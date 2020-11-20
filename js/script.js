ea5b3fcb15610e1a7651a359b7606b2b7f99cc39
Augusto Aldana <aaldanar@gmail.com>
2020-11-20 09:19:10 -0500

    Verification de la creation du jeu (console.log)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+        //Le jeu
+
+    class Jeu {
+        constructor() {
+
+            console.log("Creation du jeu");
+        }
+    }
+
+
+
+        //serpent
+
+    class Serpent{
+        constructor() {
+            console.log("Creation du serpent");
+        }
+    }
+
+        //la pomme
+
+    class Pomme{
+        constructor() {
+            console.log("Creation de la pomme");
+        }
+    }
+
+
+    var unePartie = new Jeu();
+
+
+
 
 });
